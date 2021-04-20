2d21596bdadd516c942431b14f8eab06f6a8230b
sbrevolution5 <sbrevolution5@aim.com>
2021-04-20 10:58:30 -0400

    removed unneccesary css and added code block, scaffolded pseudocode

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
+class Task {
+    constructor(name, dueDate){
+        this.name=name,
+        this.dueDate= dueDate,
+        this.createDate = new Date(),
+        this.done = false,
+        this.id = Math.random()*100 //Need unique ids for each task.  TODO: fix
+    }
+}
 //Setup page
-// 
+//load task list
+//create new
+//edit title/date
+//delete task
+//mark task complete (or not complete?)
+//filter tasks by complete or not(?)
